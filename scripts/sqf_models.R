e9b7fe4afec3ef17f68ba6e626534aa7d3c7119c
sr4918 <sr4918@nyu.edu>
2019-10-29 10:08:15 -0400

    done and changed directories

--- scripts/sqf_models.R
+++ scripts/sqf_models.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 library(ROCR)
 library(ggplot2)
-setwd("C:/Users/samee/Dropbox/NYU-PhD/3. Fall 2019/Messy Data and ML/Assignment 4")
-#QB1_2
+#setwd("C:/Users/samee/Dropbox/NYU-PhD/3. Fall 2019/Messy Data and ML/Assignment 4")
+#QB1_1
       #set the seed to2048.
       set.seed(2048)
       
@@ -30,7 +30,8 @@ setwd("C:/Users/samee/Dropbox/NYU-PhD/3. Fall 2019/Messy Data and ML/Assignment 
                                           filter(complete.cases(.))
 #QB1_3  
       #3.Randomly shuffle   the rows of sqf_dataand split it into training, validation, and test sets,
-      #using a60%-20%-20% split. Call the resulting tibbles train_sqf,validation_sqf, and test_sqf.                                          
+      #using a60%-20%-20% split. Call the resulting tibbles train_sqf,validation_sqf, and test_sqf.  
+      set.seed(2048)
       sqf_data<-sample_n(sqf_data, n())
 
       temp<-sample(1:nrow(sqf_data),.6*nrow(sqf_data))
@@ -69,34 +70,42 @@ setwd("C:/Users/samee/Dropbox/NYU-PhD/3. Fall 2019/Messy Data and ML/Assignment 
         auc_ROCR_B2 <- performance(ROCR_B2, measure = "auc")
         return (auc <- auc_ROCR_B2@y.values[[1]])
       }
-  # t<- get_auc(model_performance[1,1],model_performance[1,2])
+      
+    
+     # model_performance2<-head(model_performance)
+   #   sapply(model_performance2, FUN =get_auc2())
+      
+      
       for ( i in 1:nrow(model_performance))
       {
-          model_performance[i,3]<-get_auc(model_performance[i,1], model_performance[i,2])
+        model_performance[i,3]<-get_auc(model_performance[i,1], model_performance[i,2])
       }
 
       names(model_performance)<-c("feature_one", "feature_two","validation_auc")
       max_auc <-max(model_performance$validation_auc)
       maxindex <- which(model_performance$validation_auc== max_auc)
-      
-      #33, 75.48
+      maxindex
+      max_auc
+    #  write.csv(model_performance, "model_performance.csv")
+      #33, 76.86)
       
 #B2.3
-      # Combine train_sqf andvalidation_sqf and fit a logistic regression model on this combined dataset using 
+      # Combine train_sqf and validation_sqf and fit a logistic regression model on this combined dataset using 
       # the same triplet of features that mazimized the AUC in the previous step. Report the AUCobtained when
       # making predictions with this new model ontest_sqf. Note that although this modeluses the same features 
       # as the model from the previous step, it will have different coefficients since it istrained on more data.
        
       B2.3_data<-rbind(train_sqf, validation_sqf)
       X1<-model_performance[maxindex,1]
-        X2<-model_performance[maxindex,2]
+      X2<-model_performance[maxindex,2]
       modelB2.3 <- glm(formula = paste("found.weapon ~", X1," +",X2, "+ precinct"), family = binomial, data = B2.3_data)
       pred_B2.3 <- predict(modelB2.3, test_sqf, type = "response")
       ROCR_B2.3 <- prediction(pred_B2.3, test_sqf$found.weapon)
       auc_ROCR_B2.3 <- performance(ROCR_B2.3, measure = "auc")
       this_auc<-auc_ROCR_B2.3@y.values[[1]]
+      this_auc
+      #75.57%
       
-      #75.22%
 #B2.4 Plot a histogram of the 528 validation set AUC scores from Question B2.1, with a solid red
       #vertical line illustrating the AUC you obtained in question B2.3 (the "best" three-feature model
       #trained on 80%of the data), and a dotted red vertical line illustrating the AUC you obtained for the corresponding model 
@@ -105,8 +114,8 @@ setwd("C:/Users/samee/Dropbox/NYU-PhD/3. Fall 2019/Messy Data and ML/Assignment 
         geom_vline(xintercept=max_auc, linetype="solid", color = "red")+
       geom_vline(xintercept = this_auc, linetype = "dashed", color = "red")+
         labs(title="Histogram for AUC of different models", x="validation_AUC", y="Count")
-      ggsave("hw4_Reza/figures/question_b2.png", myhistogram)
-      
+      ggsave("hw4_reza_chase_tello/figures/question_b2.png", myhistogram)
+      myhistogram
 
 #B3
       #B3.1
@@ -119,7 +128,7 @@ setwd("C:/Users/samee/Dropbox/NYU-PhD/3. Fall 2019/Messy Data and ML/Assignment 
       #B3.2
       #2. Randomly shuffle sqf_pre_2015 and split it in half, storing the results as sqf_pre_train and
       #sqf_pre_test.
-      
+      set.seed(2048)
       sqf_pre_2015<-sample_n(sqf_pre_2015, n())
       
       index<-sample(1:nrow(sqf_pre_2015),.5*nrow(sqf_pre_2015))
@@ -144,13 +153,15 @@ setwd("C:/Users/samee/Dropbox/NYU-PhD/3. Fall 2019/Messy Data and ML/Assignment 
       ROCR_B3.4 <- prediction(pred_B3.4, sqf_pre_test$found.weapon)
       auc_ROCR_B3.4 <- performance(ROCR_B3.4, measure = "auc")
       auc_B3.4 <- auc_ROCR_B3.4@y.values[[1]]
-      # value is 82.2%
+      auc_B3.4 
+      # value is 81.61%
       
       pred_B3.4_b <- predict(modelB3.3, sqf_2015, type = "response")
       ROCR_B3.4_b <- prediction(pred_B3.4_b, sqf_2015$found.weapon)
       auc_ROCR_B3.4_b <- performance(ROCR_B3.4_b, measure = "auc")
       auc_B3.4_b <- auc_ROCR_B3.4_b@y.values[[1]]
-      #value is 75.46%
+      auc_B3.4_b 
+      #value is 75.02%
       
 
 #QB4
@@ -214,14 +225,8 @@ setwd("C:/Users/samee/Dropbox/NYU-PhD/3. Fall 2019/Messy Data and ML/Assignment 
       #Make a plot with these AUC scores on the y-axis and the year on the x-axis. Save this figure to
       #figures/question_b4.png in your submission. In your writeup, report what you see in this plot, and
       #why this might occur.
-      myhistogram<-
-        geom_vline(xintercept=max_auc, linetype="solid", color = "red")+
-        geom_vline(xintercept = this_auc, linetype = "dashed", color = "red")+
-        labs(title="Histogram for AUC of different models", x="validation_AUC", y="Count")
-      ggsave("hw4_Reza/figures/question_b2.png", myhistogram)
+     
+      AUC_year <-ggplot(auc_df, aes(Year, AUC))+geom_bar(stat = "identity")
+      ggsave("hw4_reza_chase_tello/figures/question_b4.png", AUC_year)
       
-      p <-ggplot(auc_df, aes(Year, AUC))
-      p +geom_bar(stat = "identity")
-      
-      AUC_hist<-ggplot(data=auc_df, aes(Y=auc_df$) +  geom_bar(col = "blue")+
-        labs(title="Histogram for AUC of different years", x="Year", y="AUC")
+    
